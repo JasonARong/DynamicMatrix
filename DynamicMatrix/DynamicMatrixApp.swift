2b8fd66f3692e58f30cdb8bb1567649d8785ea4a
chenjiecheng <chenjiecheng@meituan.com>
2025-12-21 18:51:36 +0800

    Initial Commit

---
+++ DynamicMatrix/DynamicMatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicMatrixApp.swift
+//  DynamicMatrix
+//
+//  Created by 陈皆成 on 2025/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DynamicMatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DynamicMatrixTests/DynamicMatrixTests.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicMatrixTests.swift
+//  DynamicMatrixTests
+//
+//  Created by 陈皆成 on 2025/12/21.
+//
+
+import Testing
+@testable import DynamicMatrix
+
+struct DynamicMatrixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
